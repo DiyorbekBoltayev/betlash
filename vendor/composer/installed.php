2f4d836a83d3de38f426b3dac964c6d83298b940
Diyorbek Boltayev <boltayevdiyorbek017@gmail.com>
2024-03-26 16:38:02 +0500

    deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2d732e47d8e76452f62b1e9a77fa84b5d27ccca0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2d732e47d8e76452f62b1e9a77fa84b5d27ccca0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
